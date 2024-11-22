ce6adf2662a8be3144477975f70121d13d45f24c
B Shrinidhi <b.shrinidhi1214@gmail.com>
2024-11-22 12:41:47 +0530

    auth middleware created - rbac remaining

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken'
+
+const authenticate = (req, res, next) => {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized: No token provided' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ error: 'Unauthorized: Invalid token' });
+        }
+
+        req.user = decoded;
+        next();
+    });
+};
+
+export default authenticate;
