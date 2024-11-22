b342b506589a3a6ffce9a067b207a22da08e95b0
B Shrinidhi <b.shrinidhi1214@gmail.com>
2024-11-22 12:25:24 +0530

    auth routes integrated in server

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import db from './models/db.js';
+import authRoutes from './routes/authRoutes.js'
 
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth',authRoutes)
 
 app.listen(PORT, ()=>{
     console.log("server running...")
