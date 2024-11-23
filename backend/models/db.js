10c3e85b4dfc256b55e327fb4270bb4fd647db15
B Shrinidhi <b.shrinidhi1214@gmail.com>
2024-11-23 07:29:00 +0530

    added hidden field in user

--- backend/models/db.js
+++ backend/models/db.js
@@ -22,6 +22,7 @@ const db = new sqlite3.Database(dbPath, (err)=>{
             curr_level INT DEFAULT 1,
             hint_taken BOOLEAN DEFAULT 0,
             curr_keys INT DEFAULT 3,
+            hidden BOOLEAN default 0,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )`);
 
