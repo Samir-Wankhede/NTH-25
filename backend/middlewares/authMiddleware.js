1d28e001c2d0cb9bc1d6bb26f12d98a22f87fba4
Samir Wankhede <samirsw04@gmail.com>
2025-01-31 18:00:22 +0530

    middleware code to remove token

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -4,12 +4,13 @@ import jwt from 'jsonwebtoken';
 const authenticate = () => (req, res, next) => {
     const token = req.cookies?.token; 
     if (!token) {
+        res.clearCookie('token');
         return res.status(401).json({ error: 'Unauthorized: No token provided' });
     }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-           
+            res.clearCookie('token');
             if (err.name === 'TokenExpiredError') {
                 return res.status(401).json({ error: 'Unauthorized: Token has expired' });
             }
