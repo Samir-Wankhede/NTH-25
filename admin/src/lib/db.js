58509d045f60b77c7430f25112c96d04866405aa
Samir Wankhede <samirsw04@gmail.com>
2025-01-31 11:07:35 +0530

    dynamic db path set

--- admin/src/lib/db.js
+++ admin/src/lib/db.js
@@ -1,7 +1,7 @@
 import sqlite3 from "sqlite3";
 import path from "path";
 
-const dbPath = path.join(process.cwd(), "../data", "nth-db.sqlite");
+const dbPath = process.env.DATABASE_PATH || path.join(process.cwd(), "/data", "nth-db.sqlite");
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) console.error("Database Error:", err.message);
   else console.log("DB Connected");
