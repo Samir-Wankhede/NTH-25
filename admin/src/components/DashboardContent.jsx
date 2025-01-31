e2bbd018cad0ff80252f04f133a85e24a6c5a879
Samir Wankhede <samirsw04@gmail.com>
2025-01-31 15:35:21 +0530

    isolated frontend

--- admin/src/components/DashboardContent.jsx
+++
@@ -1,9 +0,0 @@
-const DashboardContent = () => {
-  return (
-    <div className=' w-screen h-screen flex justify-center items-center pb-24'>
-      <p className="md:text-9xl text-5xl">NTH Admin</p>
-    </div>
-  )
-}
-
-export default DashboardContent
--- backend/routes/leaderboardRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import { getLeaderBoard } from '../controllers/leaderboardController.js'
-const router = express.Router()
-
-router.get('/',getLeaderBoard )
-
-export default router;
