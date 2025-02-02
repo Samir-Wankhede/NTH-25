9ec49a86984acf70194e7d4c60e6a1c8596ce739
Samir Wankhede <samirsw04@gmail.com>
2025-02-02 20:39:32 +0530

    changed docker compose

--- data-init/entrypoint.sh
+++ data-init/entrypoint.sh
@@ -2,3 +2,11 @@
 echo "Creating /data directory..."
 mkdir -p /data
 echo "✅ /data directory created successfully!"
+echo "Creating /etc/letsencrypt directory..."
+mkdir -p /etc/letsencrypt
+chmod -R 755 /etc/letsencrypt
+echo "✅ /data directory created successfully!"
+echo "Creating /var/www/certbot directory..."
+mkdir -p /var/www/certbot
+chmod -R 755 /var/www/certbot
+echo "✅ /data directory created successfully!"
