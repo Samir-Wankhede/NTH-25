ba4c28ba88c41c4f4e0f0c351f6f6a220120fe4a
Samir Wankhede <samirsw04@gmail.com>
2025-02-02 22:58:02 +0530

    compact dockerfile

---
+++ nginx-certbot/wait-for-certbot.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Start nginx
+nginx
+
+# Wait for the certbot_finished file to appear
+while [ ! -f /var/www/certbot/certbot_finished ]; do
+    echo "Waiting for certbot to finish..."
+    sleep 5
+done
+
+echo "Certbot finished, shutting down nginx-certbot..."
+nginx -s quit
+
+# Wait a bit to ensure nginx has time to shutdown gracefully
+sleep 5
