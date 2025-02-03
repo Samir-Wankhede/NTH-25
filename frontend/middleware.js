997975df4b73b4926ec90a1136c0cbe7a8ee5fc5
Samir Wankhede <samirsw04@gmail.com>
2025-02-04 01:24:58 +0530

    fixed build issue

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,14 +1,11 @@
 import { NextResponse } from 'next/server';
 
-const isAuthenticated = (req) => {
-  const userToken = req.cookies.get('token') || null;
-  return userToken === null;
-};
-
 export function middleware(req) {
   const { pathname } = req.nextUrl;
+  const tokenCookie = req.cookies.get('token');
+  const isAuthenticated = tokenCookie !== undefined;
 
-  if (isAuthenticated(req) && !pathname.startsWith('/login')) {
+  if (!isAuthenticated && !pathname.startsWith('/login')) {
     const url = new URL('/login', req.url);
     url.searchParams.set('unauthenticated', 'true');
     return NextResponse.redirect(new URL( url));
