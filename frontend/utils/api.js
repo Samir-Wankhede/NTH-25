a84890095fbf741b5239399f31edb062850a4f47
Samir Wankhede <samirsw04@gmail.com>
2025-02-04 15:20:43 +0530

    passed env to dockerfile in frontend

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'https://nthhost.credenz.co.in',
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'https://nthhost.credenz.co.in/backend/server/api',
     withCredentials: true
 });
 
