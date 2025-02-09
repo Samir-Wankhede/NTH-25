a5d89966717f4c2e92b3693b82e8527bfcff3478
Samir Wankhede <samirsw04@gmail.com>
2025-02-09 14:28:14 +0530

    ip proxy trusted

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000
 
-
+app.set('trust proxy', true);
 app.use(cors({origin:["https://nth.credenz.co.in"],credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 app.use((req,res,next)=>{
-    console.log('user Agent:',req.headers['user-agent'], 'user Origin:',req.headers['origin'], 'IP: ',req.ips);
+    console.log('user Agent:',req.headers['user-agent'], 'user Origin:',req.headers['origin'], 'IP: ',req.ip);
     console.log(req.method,' ',req.url)
     next()
 })
