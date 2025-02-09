d4ae95eaf42ae683fbd1b4cce90a8b1dec874015
Samir Wankhede <samirsw04@gmail.com>
2025-02-09 14:26:01 +0530

    ip showing updated

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cors({origin:["https://nth.credenz.co.in"],credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 app.use((req,res,next)=>{
-    console.log('user Agent:',req.headers['user-agent'], 'user Origin:',req.headers['origin'], 'IP: ',req.ip);
+    console.log('user Agent:',req.headers['user-agent'], 'user Origin:',req.headers['origin'], 'IP: ',req.ips);
     console.log(req.method,' ',req.url)
     next()
 })
