ef9e752e4f95c5549d5f0552d71bb40c4b51daf6
B Shrinidhi <b.shrinidhi1214@gmail.com>
2025-02-14 14:32:26 +0530

    api deleted

--- frontend/utils/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const API = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'https://nthhost.credenz.co.in/backend/server/api',
-    withCredentials: true
-});
-
-
-export default API;
